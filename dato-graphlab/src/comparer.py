46d0cac18ab8ff7a4aad36e5a62c0fe30fda9620
warreee <wardschodts@hotmail.com>
2016-07-07 15:32:27 +0200

    added python pagerank results

--- dato-graphlab/src/comparer.py
+++ dato-graphlab/src/comparer.py
@@ -1,6 +1,6 @@
 import graphlab as gl
 import config
-
+import graphlab.aggregate as agg
 
 def compare(correctList, newList, scope):
     """
@@ -17,37 +17,58 @@ def compare(correctList, newList, scope):
     A double which represent the percentage of comparison.
     """
     correct = 0
-    if len(correctList) != len(newList):
-        raise ValueError("List must be of equal size")
+    notfound = 0
+    if len(correctList) < len(newList):
+        raise ValueError("Size of newList should be smaller than the correctlist")
     else:
         for item in newList:
-            low = correctList.index(item) - scope
-            high = correctList.index(item) + scope
+            try:
+                low = correctList.index(item) - scope
+                high = correctList.index(item) + scope
 
-            if low < 0:
-                low = 0
-            if high > len(correctList) - 1:
-                high = len(correctList) - 1
+                if low < 0:
+                    low = 0
+                if high > len(correctList) - 1:
+                    high = len(correctList) - 1
 
-            if low <= newList.index(item) <= high:
-                correct += 1
-
-        return float(correct) / len(correctList)
-
-
-newList = list()
-new = gl.SFrame.read_csv(config.getDataPath() + "sample-small.formatted.txt", delimiter=",", header=False).sort('X2', ascending=False)[
-        'X1']
-for i in new:
-    newList.append(i)
-
-correctList = list()
-correct = \
-    gl.SFrame.read_csv(config.getOutputFolder() + "sample-small.pagerank.txt", delimiter='\t', header=False,
-                       skiprows=6)[
-        'X2']
-for i in correct:
-    correctList.append(i)
+                if low <= newList.index(item) <= high:
+                    correct += 1
+            except ValueError:
+                notfound += 1
+        return float(correct) / (len(newList) - notfound)
 
 
-print compare(correctList, newList, 5)
+dataSets = ['small', 'medium', 'large']
+
+for d in dataSets:
+
+    newList = list()
+
+    data = gl.SFrame.read_csv(config.getOutputFolder() + d + ".csv", delimiter=",", header=False)
+
+
+
+
+    new = data.groupby(key_columns='X2', operations={'X1': agg.SUM('X1')}).sort('X2', ascending=False)['X1']
+
+    for i in new:
+        newList.append(i)
+
+    correctList = list()
+    correct = \
+        gl.SFrame.read_csv(config.getOutputFolder() + "sample-" + d + ".pagerank.txt", delimiter='\t', header=False,
+                           skiprows=6).groupby(key_columns='X1', operations={'X2': agg.SUM('X2')}).sort('X1', ascending=False)[
+            'X2']
+    for i in correct:
+        correctList.append(i)
+    print ""
+
+
+    with open(config.getOutputFolder() + d + "Comparison.txt", 'w') as f:
+        f.write(str(0) + ',' + str(compare(correctList, newList, 0)) + '\n')
+        f.write(str(1) + ',' + str(compare(correctList, newList, 1)) + '\n')
+        f.write(str(2) + ',' + str(compare(correctList, newList, 2)) + '\n')
+        f.write(str(3) + ',' + str(compare(correctList, newList, 3)) + '\n')
+        f.write(str(4) + ',' + str(compare(correctList, newList, 4)) + '\n')
+        f.write(str(5) + ',' + str(compare(correctList, newList, 5)) + '\n')
+        f.close()
--- dato-graphlab/src/pagerank.py
+++ dato-graphlab/src/pagerank.py
@@ -4,18 +4,17 @@ import config
 small = gl.SFrame.read_csv(config.getDataPath() + "sample-small.formatted.txt", delimiter="\t", header=False)
 medium = gl.SFrame.read_csv(config.getDataPath() + "sample-medium.formatted.txt", delimiter="\t", header=False)
 large = gl.SFrame.read_csv(config.getDataPath() + "sample-large.formatted.txt", delimiter="\t", header=False)
-#stanford = gl.SFrame.read_csv(config.getStanford(), delimiter="\t", header=False)
-#google = gl.SFrame.read_csv(config.getGoogle(), delimiter="\t", header=False)
+stanford = gl.SFrame.read_csv(config.getStanford(), delimiter="\t", header=False)
+google = gl.SFrame.read_csv(config.getGoogle(), delimiter="\t", header=False)
 
-data = {'small': small, 'medium': medium, 'large': large}
+data = {'small': small, 'medium': medium, 'large': large, 'stanford': stanford, 'google': google}
 
 for d in data.keys():
-
     g = gl.graphlab.SGraph()
 
     g = g.add_edges(data.get(d), src_field='X1', dst_field='X2')
 
-    pr = gl.graphlab.pagerank.create(g, max_iterations=100)
+    pr = gl.graphlab.pagerank.create(g, max_iterations=100, threshold=0.01)
 
     scores = pr.get('pagerank').remove_column('delta').sort('pagerank', ascending=False)
 
@@ -27,3 +26,10 @@ for d in data.keys():
         headerData = fin.read().splitlines(True)
     with open(name, 'w') as fout:
         fout.writelines(headerData[1:])
+
+    params = config.getOutputFolder() + d + '.info'
+
+    with open(params, 'w') as f:
+        f.write("training_time = " + str(pr.training_time) + '\n')
+        f.write("num_iterations = " + str(pr.num_iterations) + '\n')
+        f.close()
